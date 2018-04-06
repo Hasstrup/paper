e15847eedbb858d539b0ab55eee55c3af7dc0415
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2018-04-06 14:52:16 +0100

    fixed pushresource bug

--- server/graphql/resources/resolver.js
+++ server/graphql/resources/resolver.js
@@ -1,6 +1,8 @@
-import ValidationError from '../../helpers/validator';
-import pushresource from '../../controllers/resources'
 
+import ValidationError from '../../helpers/validator';
+import { pushresource } from '../../controllers/resources';
+
+/* eslint no-return-await: [0] */
 export const author = async (obj, args, context) => {
   return await context.loaders.userloader.load(obj.author);
 }
@@ -9,6 +11,6 @@ export const addresource = async (obj, args) => {
   try {
     return await pushresource(args.token, args.input);
   } catch (err) {
-    throw new ValidationError(err.state)
+    throw new ValidationError(err.state);
   }
-}
+};
